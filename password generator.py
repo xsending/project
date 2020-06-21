9893d8ecfb0e06d6700452dce3d4bbca7354204e
xsending <67193379+xsending@users.noreply.github.com>
2020-06-21 15:50:53 +0800

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -3,32 +3,45 @@ import string
 
 print("Welcome to password generator")
 
+
 def main():
     letters = string.ascii_letters
     numbers = string.digits
     symbols = string.punctuation
-    number_list = None
-    letter_list = None
-    symbol_list = None
-    length = int(input("How many characters in your password?: "))
 
-    letter_input = int(input("How many letters?: "))
-    number_input = int(input("How many numbers?: "))
-    symbol_input = int(input("How many symbols?:"))
+    length = int(input("How long your will your password be?: "))
+    letter_count = int(input("How many letters?: "))
+    number_count = int(input("How many numbers?: "))
+    symbol_count = int(input("How many symbols?: "))
 
-    if length != letter_input + number_input + symbol_input:
-        print("password length and preference length do not match")
-        main()
+    if length != letter_count + number_count + symbol_count:
+        letter_list = random.choices(letters, k=letter_count)
+        number_list = random.choices(numbers, k=number_count)
+        symbol_list = random.choices(symbols, k=symbol_count)
+        
+        add = random.choice([letter_list, number_list, symbol_list])
+        fin = random.choices(add, k=length - letter_count - number_count - symbol_count)
+
+        output = letter_list + number_list + symbol_list + fin
+        random.shuffle(output)
+
+        password = ""
+        for char in output:
+            password += char
+
     else:
-        number_list = random.choices(numbers, k= number_input)
-        symbol_list = random.choices(symbols, k= symbol_input)
-        letter_list = random.choices(letters, k= letter_input)
+        letter_list = random.choices(letters, k=letter_count)
+        number_list = random.choices(numbers, k=number_count)
+        symbol_list = random.choices(symbols, k=symbol_count)
 
-    output = number_list + symbol_list + letter_list
-    random.shuffle(output)
-    password = ""
-    for char in output:
-        password += char
+        output = letter_list + number_list + symbol_list
+        random.shuffle(output)
+
+        password = ""
+        for char in output:
+            password += char
+
     print(f"Your password is: {password}")
+
 
 main()
