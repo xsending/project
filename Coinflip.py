3e1d39332ef678eea2ae903a433ea4642906cc35
xsending <67193379+xsending@users.noreply.github.com>
2020-06-25 23:03:09 +0800

    Update Coinflip.py

--- Coinflip.py
+++ Coinflip.py
@@ -6,7 +6,7 @@ streak_count0 = 0 # tracks the appearance of heads
 
 for repeat in range(10000): #repeats the 100 flips 10000 times
     for flip in range(100): #creates a coin flip 100 times
-        number = random.randint(0, 1) # 0 represents heads, 1 represents tails
+        number = random.randint(0, 1) # randomly generates either 0 or 1
         if number == 1: # checks if flip result is tails
             streak_count1 += 1 # keeps track whether tails consecutively appeared
         else:
