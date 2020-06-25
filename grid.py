47d7c241091c2640f85c6d29d7cbb0e7b9af24ac
xsending <67193379+xsending@users.noreply.github.com>
2020-06-25 22:52:14 +0800

    Create grid.py

---
+++ grid.py
@@ -0,0 +1,19 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+count = 0
+for grid_elem in grid[0]:
+    count += len(grid_elem)
+
+for y in range(count):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+        if x == 8:
+            print()
