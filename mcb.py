b07fe51dbe228e8f090d1b22560150a7689925ac
xsending <67193379+xsending@users.noreply.github.com>
2020-07-11 19:47:12 +0800

    Create mcb.py

---
+++ mcb.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# mcb.py
+
+import sys, shelve, pyperclip
+
+mcb_shelf = shelve.open('mcb')
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+    mcb_shelf[sys.argv[2]] = pyperclip.paste()
+elif len(sys.argv) == 2:
+    if sys.argv[1].lower() == 'list':
+        pyperclip.copy(str(list(mcb_shelf.keys())))
+    elif sys.argv[1] in mcb_shelf:
+        pyperclip.copy(mcb_shelf[sys.argv[1]])
+
+mcb_shelf.close()
